ae25c65ba9b3654c22a6b6bfcaea051656852b39
Buğra Görmüş <95827988+bugragormus@users.noreply.github.com>
2025-02-08 01:14:02 +0300

    troysarl.com v1.0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
-import type { AppProps } from 'next/app';
-import Layout from '../components/Layout';
-import '../styles/globals.css';
+import type { AppProps } from "next/app";
+import Layout from "../components/Layout";
+import "../styles/globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <SpeedInsights />
       <Component {...pageProps} />
     </Layout>
   );
