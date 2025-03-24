4f781797091520163ba7a178111f9728e5f76b21
Buğra Görmüş <95827988+bugragormus@users.noreply.github.com>
2025-03-24 11:11:42 +0300

    troysarl.com v3.1.1

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   siteUrl: "https://troysarl.com",
   generateRobotsTxt: true,
-  exclude: ["/super-secret-dashboard-98765/*", "/404"], // İsteğe bağlı: Tarama dışı bırakılacak sayfalar
+  exclude: [
+    "/super-secret-dashboard-98765",
+    "/super-secret-dashboard-98765/*",
+    "/404",
+  ],
+  // İsteğe bağlı: Tarama dışı bırakılacak sayfalar
   robotsTxtOptions: {
     policies: [
       { userAgent: "*", allow: "/" },
