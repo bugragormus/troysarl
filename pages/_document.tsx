b991c1e6e03e9bbe2f6ef08c7866be915e1d869d
Buğra Görmüş <95827988+bugragormus@users.noreply.github.com>
2025-05-24 18:36:55 +0300

    apple touch icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,18 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <link rel="icon" href="/fav_icon.ico" sizes="any" />
-      <body className="antialiased dark:bg-gray-900">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="icon" href="/fav_icon.ico" sizes="any" />
+          <link rel="apple-touch-icon" href="/fav_icon.ico" />
+        </Head>
+        <body className="antialiased dark:bg-gray-900">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
